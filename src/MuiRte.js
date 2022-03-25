c38f038c3e82d8e7f90700067e6921f6813240ee
Richard Hagen <richard.hagen@gmail.com>
2022-03-25 12:47:25 +0100

    Added MUI RTE styling example

--- src/MuiRte.js
+++ src/MuiRte.js
@@ -5,6 +5,23 @@ import { EditorState} from 'draft-js'
 import MUIRichTextEditor from 'mui-rte/dist/MUIRichTextEditor'
 import {Grid, Paper} from "@mui/material";
 import {stateToHTML} from "draft-js-export-html"
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+
+const codeString = `
+export default function MuiRte() {
+    const editorRef = React.useRef()
+    const [value, setValue] = React.useState(() => EditorState.createEmpty())
+    
+    return (
+        <MUIRichTextEditor 
+                ref={editorRef} 
+                onChange={setValue} 
+                label="Start typing..."
+        />
+    )
+}
+`
 
 export default function MuiRte() {
     const editorRef = React.useRef()
@@ -17,16 +34,27 @@ export default function MuiRte() {
                 <MUIRichTextEditor ref={editorRef} onChange={setValue} label="Start typing..."/>
             </Paper>
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12} sm={4}>
                 <Paper elevation={0}>
                     <pre>{JSON.stringify(value, 2, 2)}</pre>
                 </Paper>
             </Grid>
-            <Grid item xs={12} sm={6}>
-                <Paper elevation={0}>
-                    <pre>{stateToHTML(value.getCurrentContent())}</pre>
-                </Paper>
-            </Grid>
+            <Grid container item  xs={12} sm={8} direction={"column"}>
+
+                <Grid item>
+                    <Paper elevation={0}>
+                        <pre>{stateToHTML(value.getCurrentContent())}</pre>
+                    </Paper>
+                </Grid>
+                <Grid item>
+                    <Paper elevation={0}>
+                        <SyntaxHighlighter language="javascript" style={docco}>
+                            {codeString}
+                        </SyntaxHighlighter>
+                    </Paper>
+                </Grid>
+
+        </Grid>
         </Grid>
     );
 }
