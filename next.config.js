f2562cd70b37412a4b77407c313383376c44eb23
Richard Hagen <richard.hagen@gmail.com>
2022-03-25 13:00:03 +0100

    Added MUI RTE styling example

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-module.exports = {
+const withTM = require('next-transpile-modules')([
+  'react-syntax-highlighter'
+])
+
+module.exports = withTM({
   reactStrictMode: true,
   experimental: {
     emotion: {
       sourceMap: true,
       autoLabel: 'always',
     },
-  },
-}
+  }
+})
